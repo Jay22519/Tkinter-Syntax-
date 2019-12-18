5b04b05259414cc5b09af4206206957c458efc83
Jay Puri Goswami <48143124+Jay22519@users.noreply.github.com>
2019-12-18 11:17:30 +0530

    Create Status-bar.py

---
+++ Status-bar.py
@@ -0,0 +1,10 @@
+from tkinter import * 
+
+root = Tk()
+
+status = Label(root , text = "Prpearing to do nothing ...."
+              , bd=1 , relief = SUNKEN , anchor = W )
+
+status.pack(side = BOTTOM , fill = X)
+
+root.mainloop()
